ea8eeff1dd93c04b1bdaea692c0154aefda5f271
umas <1970313791@qq.com>
2023-06-24 13:19:29 +0800

    add

--- edit_tag.py
+++ edit_tag.py
@@ -7,6 +7,7 @@ tag更新
 
 import os
 import json
+import time
 
 import sys
 script_path = os.path.dirname(os.path.realpath(__file__))
@@ -32,22 +33,29 @@ for edit_file in get_file(json_dir):
         continue
     with open(os.path.join(json_dir, edit_file), "r", encoding="utf-8")as edit_jsons:
         edit_jsons = json.load(edit_jsons)
+        merged_list = {}
         for i in range(len(edit_jsons)):
+
+            # 首先合并列表
             path_old = os.path.normpath(edit_jsons[i]["path_old"].replace(store_url,""))
             path_new = os.path.normpath(edit_jsons[i]["path_new"].replace(store_url,""))
             path_new = path_new.replace("?","？")
-
             path_old = os.path.join(script_path,path_old)            
             path_new = os.path.join(script_path,path_new) 
+            merged_list[path_old] = path_new
+
+        for path_old in merged_list:
+            path_new = merged_list[path_old]
+            
             name_old = os.path.split(path_old)[-1]
             name_new = os.path.split(path_new)[-1]
 
             if os.path.isfile(path_old):
-                os.rename(path_old, path_new)
                 print("%d : %s -> %s" % (i, name_old,name_new))
+                os.rename(path_old, path_new)
             else:
                 print("file not found : %s" % path_old)
 
 
-add_new = os.path.join(script_path,"add_new.py")
-os.system("python %s" %add_new)
+# add_new = os.path.join(script_path,"add_new.py")
+# os.system("python %s" %add_new)
